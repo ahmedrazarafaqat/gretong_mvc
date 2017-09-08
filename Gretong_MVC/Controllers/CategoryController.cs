32493d56b0bf34eebedc30ca0d5169412817d27d
layanilamani <layani.lamani@gmail.com>
2017-09-08 23:00:52 +0500

    new

---
+++ Gretong_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gretong_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ShoppingCartEntities db = new ShoppingCartEntities();
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategories()
+        {
+
+            var categories = db.Categories.ToList();
+            return PartialView(categories);
+        }
+    }
+}
---
+++ Gretong_MVC/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gretong_MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        ShoppingCartEntities db = new ShoppingCartEntities();
+
+        // GET: Products
+        public ActionResult Index(int categoryid = 0)
+        {
+            var products = new List<Product>();
+
+            if (categoryid > 0)
+                products = db.Products.Where(c => c.Id == categoryid).ToList();
+            else
+                products = db.Products.ToList();
+
+            return View(products);
+        }
+    }
+}
